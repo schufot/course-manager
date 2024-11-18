46a59ab31f84b2937f02012339e12741b16da1c0
schufot <156847189+schufot@users.noreply.github.com>
2024-11-18 15:51:04 +0100

    init

---
+++ src/test/java/com/coursemanager/course_manager/CourseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursemanager.course_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
